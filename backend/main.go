0f4ad716f88a0b2b31971bc91ac64889d7ed2ace
punpunpunnawat <punnawat_punpun9559@hotmail.com>
2025-07-04 23:20:02 +0700

    config cors

--- backend/main.go
+++ backend/main.go
@@ -60,9 +60,22 @@ func main() {
 
 func middlewareCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		origin := r.Header.Get("Origin")
+
+		allowedOrigins := map[string]bool{
+			"http://localhost:5173":                        true,
+			"https://clash-and-card.punpunpunnawat.online": true,
+		}
+
+		if allowedOrigins[origin] {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+		} else {
+			//
+		}
+
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
 		w.Header().Set("Vary", "Origin")
 
 		if r.Method == "OPTIONS" {
